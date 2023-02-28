e7bb746f3aab4c43c35f21da2b7803bb62f91fbe
Ray Smets <rayjsmets@gmail.com>
2023-02-27 23:50:04 -0800

    api

--- app/coreAPI.server.ts
+++ app/coreAPI.server.ts
@@ -2,6 +2,25 @@ import { config } from '~/config';
 import { logger } from './logger.server';
 
 /**
+ * Represents a credential request
+ */
+export interface CredentialRequest {
+  type: string;
+  issuers: string[];
+  required?: boolean;
+}
+
+/**
+ * Interface to encapsulate the options for the hasMatchCredentials api call
+ */
+
+export interface HasMatchCredentialsOptions {
+  email?: string;
+  phone?: string;
+  credentialRequests: CredentialRequest[];
+}
+
+/**
  * Function to make POST request to Unum ID's Core Service API /issueCredentials endpoint. The intent is to issue
  * an email credential for the Hooli application user.
  * Please note: This functionality is NOT and should NOT be called in the browser due to the sensitive nature
@@ -46,4 +65,54 @@ export const issueCredentials = async (
     `Credentials successfully issued for ${email}. Total credentials issued: ${credentials.length}`
   );
   return 'success';
+};
+
+export const hasMatchCredentials = async (
+  email: string | null
+): Promise<boolean> => {
+  if (!email) return false; // short circuit if no user email provided
+
+  const headers = {
+    Authorization: 'Bearer ' + config.unumAPIKey,
+    'Content-Type': 'application/json',
+  };
+
+  const credentialRequest: CredentialRequest = {
+    type: 'EmailCredential',
+    issuers: [], // any issuer accepted
+    required: true,
+  };
+
+  const options: HasMatchCredentialsOptions = {
+    email,
+    credentialRequests: [credentialRequest],
+  };
+
+  const body = JSON.stringify(options);
+
+  try {
+    const response = await fetch(
+      config.coreServiceUrl + '/hasMatchingCredentials',
+      {
+        method: 'POST',
+        headers,
+        body,
+      }
+    );
+    const result = await response.json();
+
+    if (result?.code) {
+      logger.debug(
+        `No credentials issued for ${email}. Error: ${result.message}`
+      );
+      return false;
+    }
+
+    logger.info(`Has matching credentials? ${result.match}.`);
+
+    return result.match;
+  } catch (e) {
+    logger.error(`issueCredentials for ${email} failed. Error: ${e}`);
+    throw e;
+  }
 };
