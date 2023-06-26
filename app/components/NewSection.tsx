7adfba23dcaf168c9480432aef30877435b022f3
Iagor Moraes <iagor2014@outlook.com>
2023-06-26 10:25:57 -0300

    chore(dashboard): update new section layout

--- app/components/NewSection.tsx
+++ app/components/NewSection.tsx
@@ -11,57 +11,81 @@ export default () => {
         variant='h2'
         color='primary.dark'
         fontWeight={900}
-        sx={{ mt: 2, ml: 2 }}
+        sx={{ mt: 2, ml: 2, mb: 2 }}
       >
         New
       </Typography>
-      <Box sx={{ mx: 2, mt: 2, mb: 0, position: 'relative' }}>
-        <Typography
-          variant='h2'
-          color='primary.contrastText'
-          position='absolute'
-          right={21}
-          top={18}
-        >
-          Get $25 free!
-        </Typography>
-        <img
-          style={{ marginBottom: 0 }}
-          width='100%'
-          src='/man-holding-phone.svg'
-        />
-      </Box>
       <Box
         sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            sm: 'row',
+          },
           bgcolor: 'white',
-          pt: 2,
+          pt: { xs: 4, sm: 8 },
           pb: 1,
           mx: 2,
           mb: 1,
-          mt: -1,
-          borderRadius: '0 0 8px 8px',
+          borderRadius: '8px',
         }}
       >
-        <Typography
-          variant='h5'
-          color='primary.dark'
-          sx={{ mb: 1, mx: 2 }}
-          fontWeight={900}
-        >
-          Invite Your Friends and Earn!
-        </Typography>
-        <Typography variant='body2' color='primary.dark' sx={{ mb: 1, mx: 2 }}>
-          For every friend you refer, you'll receive $5. You can earn up to $25
-          total.
-        </Typography>
-        <Typography
-          variant='h5'
-          color='primary.main'
-          sx={{ mb: 1, mx: 2 }}
-          fontWeight={900}
-        >
-          Invite Now
-        </Typography>
+        <Box sx={{ flex: 1, flexShrink: 0, px: 8 }}>
+          <Box
+            sx={{
+              mx: { xs: 1, sm: 2 },
+              mb: { xs: 4, sm: 0 },
+              position: 'relative',
+            }}
+          >
+            <Typography
+              variant='h1'
+              color='primary.dark'
+              right={21}
+              top={18}
+              sx={{ textAlign: 'center', fontWeight: 900 }}
+            >
+              Grow Your 401(k) on Autopilot
+            </Typography>
+            <Typography
+              variant='body2'
+              color='primary.main'
+              sx={{ textAlign: 'center', my: 2 }}
+            >
+              And get a $25 bonus!
+            </Typography>
+            <Typography
+              variant='body2'
+              color='primary.dark'
+              sx={{ mb: 1, mx: 2, mt: 4, textAlign: 'center' }}
+            >
+              We've partnered with Lentable so you can get a cash advance to
+              meet your employer's 401(k) match.
+            </Typography>
+            <Typography
+              variant='body2'
+              color='primary.dark'
+              sx={{ mb: 1, mx: 2, mt: 4, textAlign: 'center' }}
+            >
+              With your Hooli ID card, you can sign up through the fast lane:
+            </Typography>
+            <Box sx={{ mt: 4 }}>
+              <img
+                src='/activate_button_with_powered_by_(Hooli).png'
+                style={{ width: '100%' }}
+              />
+            </Box>
+          </Box>
+        </Box>
+        <Box sx={{ flex: 1, flexShrink: 0, px: 8, pb: { xs: 4, sm: 8 } }}>
+          <Box sx={{ mb: 2 }}>
+            <img
+              src='/activate_button_with_powered_by_(Hooli).png'
+              style={{ width: '100%' }}
+            />
+          </Box>
+          <img src='/id-card-hooli.png' style={{ width: '100%' }} />
+        </Box>
       </Box>
     </Box>
   );
